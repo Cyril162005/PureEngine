107462487f1407c7cc3809f799ec9144a7d31dae
Cyril <cyril@example.com>
2026-08-30 23:31:27 +0000

    PureEngine Step 25: use GLFW_TRUE for forward-compatibility hint

--- src/main.cpp
+++ src/main.cpp
@@ -542,7 +542,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 #endif
 
     // 3. Create the Window
